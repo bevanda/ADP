184b4a65d5e583ca43e775288d099db8db0acb45
pbevanda <petar.bevanda@live.com>
2019-01-10 14:55:07 +0100

    Value Iteration fixed delta error

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from ADP.utilities import *
 import sys
 
 
-def value_iteration(env, theta=0.01, discount_factor=0.9):
+def value_iteration(env, theta=0.001, discount_factor=0.9):
     """
     Value Iteration Algorithm.
 
@@ -43,7 +43,7 @@ def value_iteration(env, theta=0.01, discount_factor=0.9):
             A = one_step_lookahead(s, V)
             best_action_value = np.min(A)
             # Calculate delta across all states seen so far
-            delta = min(delta, np.abs(best_action_value - V[s]))
+            delta = max(delta, np.abs(best_action_value - V[s]))
             # Update the value function. Ref: Sutton book eq. 4.10.
             V[s] = best_action_value
             # Check if we can stop
@@ -62,102 +62,14 @@ def value_iteration(env, theta=0.01, discount_factor=0.9):
     return policy, V
 
 
-def policy_eval(policy, env, discount_factor=0.9, theta=0.01):
-
-    # Start with a random (all 0) value function
-    V = np.zeros(env.num_states)
-    while True:
-        delta = 0
-        # For each state, perform a "full backup"
-        for s in range(env.num_states):
-            v = 0
-            # Look at the possible next actions
-            for a, action_prob in enumerate(policy[s]):
-                # For each action, look at the possible next states...
-                for prob, next_state, cost in env.P_g[s][a]:
-                    # Calculate the expected value
-                    v += action_prob * prob * (cost + discount_factor * V[next_state])
-            # How much our value function changed (across any states)
-            delta = max(delta, np.abs(v - V[s]))
-            V[s] = v
-        # Stop evaluating once our value function change is below a threshold
-        if delta < theta:
-            break
-    return np.array(V)
-
-
-def policy_improvement(env, policy_eval_fn=policy_eval, discount_factor=0.9):
-    """
-    Policy Improvement Algorithm. Iteratively evaluates and improves a policy
-    until an optimal policy is found.
-
-    Args:
-        env: The OpenAI envrionment.
-        policy_eval_fn: Policy Evaluation function that takes 3 arguments:
-            policy, env, discount_factor.
-        discount_factor: gamma discount factor.
-
-    Returns:
-        A tuple (policy, V).
-        policy is the optimal policy, a matrix of shape [S, A] where each state s
-        contains a valid probability distribution over actions.
-        V is the value function for the optimal policy.
-
-    """
-
-    def one_step_lookahead(state, V):
-        """
-        Helper function to calculate the value for all action in a given state.
-
-        Args:
-            state: The state to consider (int)
-            V: The value to use as an estimator, Vector of length env.num_states
-
-        Returns:
-            A vector of length env.num_actions containing the expected value of each action.
-        """
-        A = np.zeros(env.num_actions)
-        for a in range(env.num_actions):
-            for prob, next_state, cost in env.P_g[state][a]:
-                A[a] += prob * (cost + discount_factor * V[next_state])
-        return A
-
-    # Start with a random policy
-    policy = np.ones([env.num_states, env.num_actions]) / env.num_actions
-
-    while True:
-        # Evaluate the current policy
-        V = policy_eval_fn(policy, env, discount_factor)
-
-        # Will be set to false if we make any changes to the policy
-        policy_stable = True
-
-        # For each state...
-        for s in range(env.num_states):
-            # The best action we would take under the correct policy
-            chosen_a = np.argmin(policy[s])
-
-            # Find the best action by one-step lookahead
-            # Ties are resolved arbitrarily
-            action_values = one_step_lookahead(s, V)
-            best_a = np.argmin(action_values)
-
-            # Greedily update the policy
-            if chosen_a != best_a:
-                policy_stable = False
-            policy[s] = np.eye(env.num_actions)[best_a]
-
-        # If the policy is stable we've found an optimal policy. Return it
-        if policy_stable:
-            return policy, V
 
 
 if __name__ == "__main__":
 
     env = Maze()
 
-    #print value_iteration(env)
-    print policy_improvement(env)
+    print value_iteration(env)
+    #print policy_improvement(env)
 
 
 
